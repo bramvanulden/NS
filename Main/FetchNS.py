eb094f48b6d1322b661807a3b0b67c8906057745
bramvanulden <42969037+bramvanulden@users.noreply.github.com>
2018-10-16 23:51:23 +0200

    Bericht gesplit #TODO: bugfix <b> weghalen(werkt niet in dict)

--- Main/FetchNS.py
+++ Main/FetchNS.py
@@ -1,8 +1,11 @@
 import requests
 import xmltodict
+from xml.etree import ElementTree as ET
+
 
 writefile = open("dump.xml", "w")
 auth_details = ('bramvanulden@live.nl', 'U-8hSW5D50saIgMxCNFI7NpVxGC_nlcEKMKNRm4s2U6SkhdYxOchjw')
+readfile = open("dump.xml", "r")
 
 def getVertrektijden(station):
     d = {}
@@ -18,4 +21,50 @@ def getVertrektijden(station):
         vertrekspoorStr = vertrek["VertrekSpoor"]
         vertrekspoor = vertrekspoorStr.get("#text")
 
+        # IN DICTIONARY:
+        #   vertrekTijd  = 0
+        #   ritnummer    = 1
+        #   treinsoort   = 2
+        #   vertrekspoor = 3
+
         d[eindbestemming] = [vertrekTijd, ritnummer, treinsoort, vertrekspoor]
+    return d
+
+
+def getStoringenWerkzaamheden(station):
+    d = {}
+    wanneer = ""
+    oorzaak = ""
+    advies = ""
+    nieuwedienstregeling = ""
+    extrareistijd = ""
+    i = 0
+    api_station = "http://webservices.ns.nl/ns-api-storingen?station={0}".format(station)
+    response_station = requests.get(api_station, auth=auth_details)
+    StoWernXML = xmltodict.parse(response_station.text)
+    for werkzaamheed in StoWernXML["Storingen"]["Gepland"]["Storing"]:
+        traject = werkzaamheed["Traject"]
+        periode = werkzaamheed["Periode"]
+        bericht = werkzaamheed["Bericht"]
+
+        with open("dump.xml", "w") as myXMLFile:
+            myXMLFile.write(werkzaamheed["Bericht"])
+
+        for line in readfile:
+            i += 1
+            if "Wanneer:" in line:
+                wanneer = line[11:-10]
+            if "Oorzaak" in line:
+                oorzaak = line[11:-10]
+            if "Advies" in line:
+                advies = line[10:-5]
+            if i == 5:
+                nieuwedienstregeling = line[5:]
+            if "Extra reistijd:" in line:
+                extrareistijd = line[20:-10]
+
+        d[traject] = [periode, bericht, wanneer, oorzaak, advies, nieuwedienstregeling, extrareistijd]
+
+
+print(getStoringenWerkzaamheden("ut"))
+
